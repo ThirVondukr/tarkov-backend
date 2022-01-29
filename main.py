44cde4b38a2fb5b13af22e44875146884ac352cc
Doctor <thirvondukr@gmail.com>
2022-01-29 06:40:00 +0300

    Format code

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import sys
 
 import uvicorn
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.path.append("src")
     uvicorn.run("app:create_app", factory=True, debug=True, reload=True)
