87e9fbde6770a0ab52e91714510e1475f71453a5
Doctor <thirvondukr@gmail.com>
2022-01-29 22:22:14 +0300

    Update uvicorn launch arguments to correctly display logs in console

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
-
 import uvicorn
 
 if __name__ == "__main__":
     sys.path.append("src")
-    uvicorn.run("app:create_app", factory=True, debug=True, reload=True)
+    uvicorn.run("app:create_app", factory=True, use_colors=False)
