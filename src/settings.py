f5de139f396ad6f9c9f5a170ac37f20bd6fbda22
Doctor <thirvondukr@gmail.com>
2022-01-31 06:54:13 +0300

    Add database settings

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
 
+import paths
+
 
 class ServerSettings(BaseModel):
     name = "JET Server"
 
 
+class DatabaseSettings(BaseModel):
+    url = f"sqlite+aiosqlite:///{paths.sqlite_db_path}"
+    run_migrations: bool = True
+
+
 server = ServerSettings()
+database = DatabaseSettings()
