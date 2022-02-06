06ec1a70d4fcde04e9710f2cce5c76fae8ace17f
Doctor <thirvondukr@gmail.com>
2022-02-06 18:33:07 +0300

    Add `/client/handbook/templates` route

--- src/modules/items/router.py
+++ src/modules/items/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends
 
+import paths
 from modules.items.repository import TemplateRepository
 from modules.items.types import Template
 from schema import Success
 from server import ZLibORJSONResponse, ZLibRoute
+from utils import read_json_file
 
 router = APIRouter(
     default_response_class=ZLibORJSONResponse,
@@ -21,3 +23,12 @@ async def client_items(
     template_repository: TemplateRepository = Depends(),
 ) -> Success[dict[str, Template]]:
     return Success(data=template_repository.templates)
+
+
+@router.post("/client/handbook/templates", response_model=Success[dict[str, list]])
+async def handbook_templates() -> Success[dict[str, list]]:
+    template_paths = paths.database.joinpath("templates").rglob("*.json")
+
+    return Success(
+        data={path.stem: await read_json_file(path) for path in template_paths}
+    )
